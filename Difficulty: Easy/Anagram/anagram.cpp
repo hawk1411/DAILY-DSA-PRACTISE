4c80930539a20e17af5a2c8c8ca8855ac28d0290
Ravi Kant Mishra <156528858+hawk1411@users.noreply.github.com>
2026-08-06 11:56:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.cpp
@@ -0,0 +1,43 @@
+class Solution {
+  public:
+    bool areAnagrams(string& s1, string& s2) {
+        // code here
+        
+        if(s1.size() != s2.size()){
+            return false;
+        }
+        int freq[26] = {0};
+        
+        for(int i=0;i<s1.size();i++){
+            
+            freq[s1[i] - 'a']++;
+            
+            
+        }
+        
+        
+        for(int i=0;i<s2.size();i++){
+            
+            freq[s2[i] - 'a']--;
+            
+        }
+        
+        for(int i=0;i<26;i++){
+            
+            if(freq[i] !=0){
+                return false;
+            }
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        return true;
+        
+    }
+};
